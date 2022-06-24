dbd4169852df927eb0eeb5828a4681a1a2ff9bef
harshitchopra7 <chopraharshit2000@gmail.com>
2022-06-24 21:26:40 +0530

    api calling and useEffect done

--- src/components/ApiCalling/ApiCalling.js
+++ src/components/ApiCalling/ApiCalling.js
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./ApiCalling.css";
 
 function ApiCalling() {
-    // https://jsonplaceholder.typicode.com/todos/
+  const [apiData, setApiData] = useState([]);
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
 
-    fetch("https://fakestoreapi.com/products")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log("Api failed, please check the api details entered are correct or not"))
+  // https://jsonplaceholder.typicode.com/todos/
+  // https://fakestoreapi.com/products
 
-    // 200 
-    // 404 - Not found
-    // 500 - Internal server error
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setApiData(data))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
-    <div>
-        Hello
+    <div className="apicalling_container">
+      {/* <label>Name</label>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <label>Surname</label>
+      <input value={surname} onChange={(e) => setSurname(e.target.value)} /> */}
+      {apiData.map((value, key) => (
+        <div>
+            <p>id:  {value.id}</p>
+          <p>Title: {value.title}</p>
+          <p>Body: {value.body}</p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default ApiCalling
+export default ApiCalling;
